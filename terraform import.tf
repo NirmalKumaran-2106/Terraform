b40ca96f75c632e76c4bae27a37bbc018d633375
NirmalKumaran-2106 <145633004+NirmalKumaran-2106@users.noreply.github.com>
2023-09-21 17:56:01 +0530

    deleted

--- terraform import.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_instance" "Import" {
-    ami_id = "ami-0ff30663ed13c2290"
-    instance_type = "t2.micro"  
-}
--- terraform stop.tf
+++
@@ -1,4 +0,0 @@
-resource "aws-instance" "Terraform" {
-    id = "i-068207abfd61b50be"
-    state = "stopped"
-}
