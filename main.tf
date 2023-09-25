f0a276c1ab0ea6ddcfeae4ec6d7ba60597528d59
NirmalKumaran-2106 <145633004+NirmalKumaran-2106@users.noreply.github.com>
2023-09-25 19:49:08 +0530

    branch created

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
-module "my-module" {
-  source = "./my-module"
+resource "aws_instance" "trial" {
+    ami = "ami-067c21fb1979f0b27"
+    instance_type = "t2.micro"
+    key_name = "keys"
+    tags = {
+        Name = "trialinstance"
+    }
 }
--- my-module/instance..tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "trial" {
-    ami = "var.ami"
-    instance_type = "var.instance_type"
-    key_name = "keys"
-    tags = {
-        Name = "var.tagname"
-    }
-}
