d528e33df68ac517b1b41f939b4e2b07a7fbec9f
NirmalKumaran-2106 <145633004+NirmalKumaran-2106@users.noreply.github.com>
2023-09-25 14:58:04 +0530

    revoked

--- main.tf
+++ main.tf
@@ -1,15 +1,10 @@
-provider "aws" {
-    region = "ap-south-1"
-    access_key = "AKIAX7GUTVZRWIL7TLOQ"
-    secret_key = "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"
-}
-resource "aws_instance" "Server" {
-    ami = "ami-067c21fb1979f0b27"
-    instance_type = "t2.micro"
-    subnet_id = "subnet-0f362b581a43ce5ce"
-    security_groups = ["sg-0715215d2df1a5a75"]
+resource "aws_instance" "Terraform_Servers" {
+    ami = "var.ami"
+    instance_type = "var.instance_type"
+    subnet_id = "var.subnet_id"
+    security_groups = ["var.security_groups"]
     key_name = "keys"
     tags = {
-        Name = "Terraform_Created_Instance"
+        Name = "var.tagname"
     } 
 }
