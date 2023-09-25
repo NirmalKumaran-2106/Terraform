936b7ddc51ec9808ccb94f09ae6018f336b36062
NirmalKumaran-2106 <145633004+NirmalKumaran-2106@users.noreply.github.com>
2023-09-25 15:05:40 +0530

    final

--- main.tf
+++ main.tf
@@ -1,9 +1,13 @@
-resource "aws_instance" "Terraform_Servers" {
+provider "aws" {
+    region = "ap-south-1"
+}
+resource "aws_instance" "Server" {
     ami = "ami-067c21fb1979f0b27"
-    instance_type = "var.instance_type"
-    subnet_id = "var.subnet_id"
+    instance_type = "t2.micro"
+    subnet_id = "subnet-0f362b581a43ce5ce"
+    security_groups = ["sg-0715215d2df1a5a75"]
     key_name = "keys"
     tags = {
-        Name = "var.tagname"
+        Name = "Terraform_Created_Instance"
     } 
 }
