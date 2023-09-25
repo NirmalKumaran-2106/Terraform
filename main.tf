d7a21efd5a7f98f4c109e0f5fafa354ca0909835
NirmalKumaran-2106 <145633004+NirmalKumaran-2106@users.noreply.github.com>
2023-09-25 13:27:43 +0530

    edited main file

--- main.tf
+++ main.tf
@@ -1,4 +1,15 @@
 resource "aws_instance" "Terraform_Servers" {
+    ami = "ami-067c21fb1979f0b27"
+    instance_type = "t2.micro"
+    subnet_id = "subnet-0f362b581a43ce5ce"
+    security_groups = ["sg-0715215d2df1a5a75"]
+    key_name = "keys"
+    tags = {
+        Name = "Terraform_Created_Instance"
+    } 
+}
+
+/*resource "aws_instance" "Terraform_Servers" {
     ami = "var.ami"
     instance_type = "var.instance_type"
     subnet_id = "var.subnet_id"
