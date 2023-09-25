8cc238efae7d5f174197086ea68b2037371410d1
NirmalKumaran-2106 <145633004+NirmalKumaran-2106@users.noreply.github.com>
2023-09-25 14:24:42 +0530

    provider file updated

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region = "ap-south-1"
-    access_key = "AKIAX7GUTVZR5VHP6D3V"
-    secret_key = "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"
+    access_key = "AKIAX7GUTVZRWIL7TLOQ"
+    secret_key = "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"
 }
