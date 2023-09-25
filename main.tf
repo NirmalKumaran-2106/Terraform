7c52944e056c59f2a393dec58cde9d7902ce5135
NirmalKumaran-2106 <145633004+NirmalKumaran-2106@users.noreply.github.com>
2023-09-25 14:54:06 +0530

    e

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
     access_key = "AKIAX7GUTVZRWIL7TLOQ"
     secret_key = "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"
 }
-resource "aws_instance" "terraform-servers" {
+resource "aws_instance" "Server" {
     ami = "ami-067c21fb1979f0b27"
     instance_type = "t2.micro"
     subnet_id = "subnet-0f362b581a43ce5ce"
