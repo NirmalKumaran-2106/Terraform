eca3e9727bf0619c26597ede37f761a4dc1d487d
NirmalKumaran-2106 <145633004+NirmalKumaran-2106@users.noreply.github.com>
2023-09-25 15:07:15 +0530

    e

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 provider "aws" {
     region = "ap-south-1"
+    access_key = "AKIAX7GUTVZRWIL7TLOQ"
+    secret_key = "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"
 }
 resource "aws_instance" "Server" {
     ami = "ami-067c21fb1979f0b27"
