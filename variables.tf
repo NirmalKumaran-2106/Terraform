d339777b21cf5e49eefb1c1e6a9b222cab9dd78b
NirmalKumaran-2106 <145633004+NirmalKumaran-2106@users.noreply.github.com>
2023-09-25 12:59:34 +0530

    variable file updated

--- variables.tf
+++ variables.tf
@@ -1,12 +1,16 @@
 variable "instance_type" {
+    type = string
     default = "t2.micro"
 }
 variable "security_groups" {
+    type = string
     default = "sg-0715215d2df1a5a75"
 }
 variable "subnet_id" {
+    type = string
     default = "subnet-0f362b581a43ce5ce"
 }
 variable "ami" {
+    type = string
     default = "ami-067c21fb1979f0b27"
 }
