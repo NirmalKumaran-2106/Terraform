0c0fb1dc6b793b16dad19153195fc0e37c0190ac
NirmalKumaran-2106 <145633004+NirmalKumaran-2106@users.noreply.github.com>
2023-09-25 15:20:49 +0530

    eee

--- main.tf
+++ main.tf
@@ -4,8 +4,6 @@ provider "aws" {
 resource "aws_instance" "Server" {
     ami = "ami-067c21fb1979f0b27"
     instance_type = "t2.micro"
-    subnet_id = "subnet-0f362b581a43ce5ce"
-    security_groups = ["sg-0715215d2df1a5a75"]
     key_name = "keys"
     tags = {
         Name = "Terraform_Created_Instance"
