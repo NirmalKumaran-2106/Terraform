c824ed578bb6af3e4fdb203495d1a5651aab2565
NirmalKumaran-2106 <145633004+NirmalKumaran-2106@users.noreply.github.com>
2023-09-25 13:31:11 +0530

    removed

--- main.tf
+++ main.tf
@@ -8,14 +8,3 @@ resource "aws_instance" "Terraform_Servers" {
         Name = "Terraform_Created_Instance"
     } 
 }
-
-/*resource "aws_instance" "Terraform_Servers" {
-    ami = "var.ami"
-    instance_type = "var.instance_type"
-    subnet_id = "var.subnet_id"
-    security_groups = ["var.security_groups"]
-    key_name = "keys"
-    tags = {
-        Name = "var.tagname"
-    } 
-}
