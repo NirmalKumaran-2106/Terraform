3ab701df0b6fd271105d4e366a882ca908ebd123
NirmalKumaran-2106 <145633004+NirmalKumaran-2106@users.noreply.github.com>
2023-09-25 14:25:09 +0530

    main file updated

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+    region = "ap-south-1"
+    access_key = "AKIAX7GUTVZRWIL7TLOQ"
+    secret_key = "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"
+}
 resource "aws_instance" "Terraform_Servers" {
     ami = "ami-067c21fb1979f0b27"
     instance_type = "t2.micro"
