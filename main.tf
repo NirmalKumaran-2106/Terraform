3df675cc80832b36d0430d47c50edd43aab2400b
GIT VSC <git_vsc@example.com>
2023-10-03 21:10:44 +0530

    add

--- main.tf
+++ main.tf
@@ -1,7 +1,14 @@
 resource "aws_instance" "trial" {
-    ami = "ami-067c21fb1979f0b27"
+    count = 3
+    ami = "ami-03d294e37a4820c21"
     instance_type = "t2.micro"
-    key_name = "keys"
+    key_name = "Dev--Ops"
+    ebs_block_device {
+    device_name           = "/dev/xvda"
+    volume_size           = 29
+    volume_type           = "gp2"
+    delete_on_termination = true
+  }
     tags = {
         Name = "trialinstance"
     }
