581d82f19832b564187f8bbb81501af198f12703
GIT VSC <git_vsc@example.com>
2023-10-08 14:51:14 +0530

    mod

---
+++ iamuser.tf
@@ -0,0 +1,52 @@
+resource "aws_iam_user" "createiamuser" {
+    name = "TerraformUser"
+}
+resource "aws_iam_group" "createiamgroup" {
+    name = "Terraform-DevOps"
+}
+resource "aws_iam_group_policy" "iamgrouppolicy" {
+    name = "Terraform-GroupPolicy"
+    group = [aws_iam_group.createiamgroup.name]
+    policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "ec2:*",
+            "Effect": "Allow",
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "s3:*",
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "lambda:*",
+            "Resource": "*"
+        }]
+    })
+}
+resource "aws_iam_role" "createrole" {
+  name = "Terraform-Role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+resource "aws_iam_group_policy_attachment" "Policy-Attachment" {
+    name = "Terraform-attachment"
+    policy_arn = aws_iam_policy.policy_arn
+    users = [aws_iam_user.createiamuser.name]
+    roles = [aws_iam_role.createrole]   
+    groups = [aws_iam_group.createiamgroup.name]
+}
